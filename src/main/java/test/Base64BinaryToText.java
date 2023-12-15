8767838e038759cc8706faf08bfd576ef700ecdb
Dan Heslinga <drdan@heslingamd.com>
2023-12-15 12:33:27 -1000

    clean up base 64 conversions

--- src/main/java/test/Base64BinaryToText.java
+++ src/main/java/test/Base64BinaryToText.java
@@ -3,36 +3,21 @@ package test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-// import org.hl7.fhir.r4.model.Base64BinaryType;
 import java.util.Base64;
 
 public class Base64BinaryToText {
-
 	public static void main(String[] args) throws IOException {
 		// Example base64Binary value
 		// String base64BinaryValue = "SGVsbG8gd29ybGQ="; // "Hello world" in base64
 		Path inPath = Path.of("./assets/base64TextIn.txt");
 		// Path outPath = Path.of("./assets/textOut.txt");
 		String inFileContent = Files.readString(inPath);
-
-		// Convert base64Binary to text
-		// String textValue = base64BinaryToText(inFileContent);
-
 		byte[] decodedBytes = Base64.getDecoder().decode(inFileContent);
 		String decodedString = new String(decodedBytes);
 		// Print the result
 		// System.out.println("Base64Binary: " + inFileContent);
 		// System.out.println("Text: " + textValue);
-
 		Path outPath = Path.of("./assets/textOut.txt");
 		Files.write(outPath, decodedString.getBytes());
 	}
-
-	// private static String base64BinaryToText(String base64BinaryValue) {
-	// // Decode base64Binary to byte array
-	// byte[] decodedBytes = Base64.getDecoder().decode(base64BinaryValue);
-
-	// // Convert byte array to String
-	// return new String(decodedBytes);
-	// }
 }
--- src/main/java/test/TextToBase64Binary.java
+++ src/main/java/test/TextToBase64Binary.java
@@ -11,23 +11,14 @@ import java.nio.file.Path;
 
 public class TextToBase64Binary {
 	public static void main(String[] args) throws IOException {
-		String textValue = "Hello world";
-
+		// String textValue = "Hello world";
+		Path inPath = Path.of("./assets/textIn.txt");
+		String textValue = Files.readString(inPath);
 		String encodedString = Base64.getEncoder().encodeToString(textValue.getBytes());
-
 		// Print the result
 		System.out.println("Text: " + textValue);
 		System.out.println("Base64Binary: " + encodedString);
-
-		// Path outPath = Path.of("./assets/base64TextOut.txt");
-		// Files.write(outPath, base64BinaryValue.getBytes());
+		Path outPath = Path.of("./assets/base64TextOut.txt");
+		Files.write(outPath, encodedString.getBytes());
 	}
-
-	// private static Base64BinaryType textToBase64Binary(String textValue) {
-	// // Encode text to base64
-	// byte[] encodedBytes =
-	// Base64.getEncoder().encode(textValue.getBytes(StandardCharsets.UTF_8));
-
-	// // Create a Base64BinaryType instance
-	// return new Base64BinaryType(encodedBytes);
 }
