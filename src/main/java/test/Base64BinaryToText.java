39797b24a44d49a71da9386dc4c17a229112f83c
Dan Heslinga <drdan@heslingamd.com>
2023-12-15 12:25:40 -1000

    create basic base64 converters

--- src/main/java/test/Base64BinaryToText.java
+++ src/main/java/test/Base64BinaryToText.java
@@ -1,66 +1,38 @@
 package test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 // import org.hl7.fhir.r4.model.Base64BinaryType;
 import java.util.Base64;
 
-// import ca.uhn.fhir.context.FhirContext;
-// import ca.uhn.fhir.rest.api.MethodOutcome;
-// import ca.uhn.fhir.rest.api.MethodOutcome;
-// import ca.uhn.fhir.rest.api.SearchStyleEnum;
-// import ca.uhn.fhir.rest.api.SummaryEnum;
-// import ca.uhn.fhir.rest.client.api.IGenericClient;
-// import ca.uhn.fhir.rest.param.DateRangeParam;
-// import ca.uhn.fhir.util.BundleUtil;
-
-// import java.util.ArrayList;
-// import java.util.List;
-
-// import org.hl7.fhir.instance.model.api.IBaseBundle;
-// import org.hl7.fhir.instance.model.api.IBaseResource;
-// import org.hl7.fhir.instance.model.api.IIdType;
-// import org.hl7.fhir.r4.model.Bundle;
-// import org.hl7.fhir.r4.model.Enumerations;
-// import org.hl7.fhir.r4.model.IdType;
-// import org.hl7.fhir.r4.model.Observation;
-// import org.hl7.fhir.r4.model.OperationOutcome;
-// import org.hl7.fhir.r4.model.Enumerations;
-// import org.hl7.fhir.r4.model.IdType;
-// import org.hl7.fhir.r4.model.Observation;
-// import org.hl7.fhir.r4.model.Reference;
-// import org.hl7.fhir.r4.model.Bundle;
-// import org.hl7.fhir.r4.model.Patient;
-// import org.hl7.fhir.r4.model.Provenance;
-// import org.hl7.fhir.r4.model.Organization;
-// import java.io.*;
-// import org.hl7.fhir.r4.model.Quantity;
-// import org.hl7.fhir.r4.model.Quantity;
-
 public class Base64BinaryToText {
 
-	public static void main(String[] args) {
-		// FhirContext ctx = FhirContext.forR4();
-		// IGenericClient client =
-		// ctx.newRestfulGenericClient("http://localhost:8080/fhir");
-
-		// public class Base64BinaryToText {
-
-		// public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// Example base64Binary value
-		String base64BinaryValue = "SGVsbG8gd29ybGQ="; // "Hello world" in base64
+		// String base64BinaryValue = "SGVsbG8gd29ybGQ="; // "Hello world" in base64
+		Path inPath = Path.of("./assets/base64TextIn.txt");
+		// Path outPath = Path.of("./assets/textOut.txt");
+		String inFileContent = Files.readString(inPath);
 
 		// Convert base64Binary to text
-		String textValue = base64BinaryToText(base64BinaryValue);
+		// String textValue = base64BinaryToText(inFileContent);
 
+		byte[] decodedBytes = Base64.getDecoder().decode(inFileContent);
+		String decodedString = new String(decodedBytes);
 		// Print the result
-		System.out.println("Base64Binary: " + base64BinaryValue);
-		System.out.println("Text: " + textValue);
+		// System.out.println("Base64Binary: " + inFileContent);
+		// System.out.println("Text: " + textValue);
+
+		Path outPath = Path.of("./assets/textOut.txt");
+		Files.write(outPath, decodedString.getBytes());
 	}
 
-	private static String base64BinaryToText(String base64BinaryValue) {
-		// Decode base64Binary to byte array
-		byte[] decodedBytes = Base64.getDecoder().decode(base64BinaryValue);
+	// private static String base64BinaryToText(String base64BinaryValue) {
+	// // Decode base64Binary to byte array
+	// byte[] decodedBytes = Base64.getDecoder().decode(base64BinaryValue);
 
-		// Convert byte array to String
-		return new String(decodedBytes);
-	}
+	// // Convert byte array to String
+	// return new String(decodedBytes);
+	// }
 }
--- src/main/java/test/TextToBase64Binary.java
+++ src/main/java/test/TextToBase64Binary.java
@@ -1,32 +1,33 @@
 package test;
 
-// import org.hl7.fhir.r4.model.Base64BinaryType;
 import java.util.Base64;
 
-import org.hl7.fhir.r4.model.Base64BinaryType;
+// import org.hl7.fhir.r4.model.Base64BinaryType;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TextToBase64Binary {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String textValue = "Hello world";
 
-		// Convert text to base64Binary
-		Base64BinaryType base64BinaryType = textToBase64Binary(textValue);
-
-		// Access the base64Binary value
-		String base64BinaryValue = base64BinaryType.getValueAsString();
+		String encodedString = Base64.getEncoder().encodeToString(textValue.getBytes());
 
 		// Print the result
 		System.out.println("Text: " + textValue);
-		System.out.println("Base64Binary: " + base64BinaryValue);
+		System.out.println("Base64Binary: " + encodedString);
+
+		// Path outPath = Path.of("./assets/base64TextOut.txt");
+		// Files.write(outPath, base64BinaryValue.getBytes());
 	}
 
-	private static Base64BinaryType textToBase64Binary(String textValue) {
-		// Encode text to base64
-		byte[] encodedBytes = Base64.getEncoder().encode(textValue.getBytes(StandardCharsets.UTF_8));
+	// private static Base64BinaryType textToBase64Binary(String textValue) {
+	// // Encode text to base64
+	// byte[] encodedBytes =
+	// Base64.getEncoder().encode(textValue.getBytes(StandardCharsets.UTF_8));
 
-		// Create a Base64BinaryType instance
-		return new Base64BinaryType(encodedBytes);
-	}
+	// // Create a Base64BinaryType instance
+	// return new Base64BinaryType(encodedBytes);
 }
